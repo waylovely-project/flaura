33d66db54a962c2c33928be41b53bb70d6488b43
Fiana Fortressia <fortressnordlys@outlook.com>
2022-10-12 17:01:51 +0700

    Implement flaura upstream add
    Signed-off-by: Nefo Fortressia <nefothingy@hotmail.com>

--- flaura/upstream.py
+++ flaura/upstream.py
@@ -2,8 +2,10 @@
 #
 # SPDX-License-Identifier: MIT OR APACHE-2.0
 
+from pathlib import Path
 import click 
 from .home import get_home
+import tomlkit
 @click.group()
 def upstream():
     """Commands to manage upstreams!! <3"""
@@ -13,17 +15,25 @@ def upstream():
 @click.argument("origin")
 @click.argument("path", required=False)
 @click.option("--home", is_flag=True)
-def add(origin, path):
+def add(origin, path, home):
     """Add an upstream! 
     
     """
     home_path = get_home()
-
-    print("This command is not yet implemented!")
-    exit(1)
-
-    with home_path.open(mode="rw") as home_fd:
+    if home:
+        path = home_path.parent()
+    else: 
+        if not path:
+            print("Please specify a path!!")
+            exit(1)
+        path = Path(path)
+    with home_path.open(mode="r") as home_fd:
         home = tomlkit.parse(home_fd.read())
+        upstream = tomlkit.table()
+        upstream["origin"] = origin
+        home.append(str(path), upstream)
 
+        with home_path.open(mode="w") as home_fd:
+            home_fd.write(tomlkit.dumps(home))
     
 upstream.add_command(add)
