08f92efdb831a4296385e5f66a92d6667390508e
Fiana Fortressia <fortressnordlys@outlook.com>
2022-10-14 18:27:32 +0700

    Initial flaura pull implementation
    Signed-off-by: Nefo Fortressia <nefothingy@hotmail.com>

--- flaura/__init__.py
+++ flaura/__init__.py
@@ -2,6 +2,7 @@
 #
 # SPDX-License-Identifier: MIT OR APACHE-2.0
 
+from flaura.pull import pull
 from .home import init
 from .upstream import add, list_sources_cmd, remove
 import click 
@@ -17,6 +18,7 @@ def flaura():
 
 flaura.add_command(merge_cmd)
 flaura.add_command(init)
+flaura.add_command(pull)
 flaura.add_command(add)
 flaura.add_command(remove)
 
---
+++ flaura/pull.py
@@ -0,0 +1,21 @@
+import click 
+from urllib.parse import urlparse
+from .home import get_home
+from .merge import merge
+import tomlkit
+
+@click.command()
+@click.option("--ours", is_flag=True)
+@click.option("--theirs", is_flag=True)
+@click.option("--union", is_flag=True)
+def pull(ours, theirs, union):
+    home_path = get_home()
+    
+    with home_path.open(mode="r") as home_fd:
+        home = tomlkit.parse(home_fd.read())
+        for upstream in home.keys():
+            if "origin" not in home[upstream]:
+                click.echo(f"{upstream}", err=True)
+                exit(1)
+            merge(home[upstream]["origin"], home_path.parent.joinpath(home[upstream].get("folder") or upstream), False, ours, theirs, union)
+            
