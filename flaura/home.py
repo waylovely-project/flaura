c4fd8bb478f67e6930056447f75cb807888a2832
Fiana Fortressia <fortressnordlys@outlook.com>
2023-03-23 14:55:14 +0700

    fix recursive issue when getting the flara.toml directory
    Signed-off-by: Fiana Fortressia <fortressnordlys@outlook.com>

--- flaura/home.py
+++ flaura/home.py
@@ -6,18 +6,18 @@ import click
 from pathlib import Path 
 import os
 from typing import Optional 
-def _get_home_from_cwd(parent: Optional[Path]):
+def _get_home_from_cwd(parent: Optional[Path], count: int):
     cwd = parent or Path.cwd()
     home = cwd.joinpath("flaura.toml")
     if home.exists():
         return home
     else:
-        if cwd.parent == Path.home() or cwd.parent == cwd.root:
+        if cwd.parent == Path.home() or cwd.parent == cwd.root or count > 5:
             click.echo("We couldn't find any flaura.toml file!! Please initiate them first by adding an upstream!!! See flaura upstream -h", err=True)
             exit(1)
-        _get_home_from_cwd(cwd.parent)
+        _get_home_from_cwd(cwd.parent, count + 1)
 def get_home():
-    return Path(os.environ.get("FLAURA_HOME") or _get_home_from_cwd(None))
+    return Path(os.environ.get("FLAURA_HOME") or _get_home_from_cwd(None, 0))
 
 
 @click.command()
