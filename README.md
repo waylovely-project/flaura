b6f5fe335091ece8b017577bd03ede248282ab28
Fiana Fortressia <fortressnordlys@outlook.com>
2023-03-23 15:35:19 +0700

    Add docs to flaura pull
    Signed-off-by: Fiana Fortressia <fortressnordlys@outlook.com>

--- README.md
+++ README.md
@@ -5,9 +5,5 @@ Flaura implements all the merging behaviors of Git, and not only that, it's Git-
 
 It allows hardforking to be like a charm!!
 
-## Project Status
-At the moment, Flaura is still in early development. 
-
-No commands have been implemented except `flaura merge` and `flaura init`. 
-
-`flaura merge` should be the basic functionality of Flaura for it to be useful, even though the UX is lacking!! You can see its documentation with `flaura merge --help`
+## Project Statu
+At the moment, Flaura is still in early development.  
--- flaura/pull.py
+++ flaura/pull.py
@@ -2,13 +2,16 @@ import click
 from urllib.parse import urlparse
 from .home import get_home
 from .merge import merge
-import tomlkit
+
 
 @click.command()
 @click.option("--ours", is_flag=True)
 @click.option("--theirs", is_flag=True)
 @click.option("--union", is_flag=True)
 def pull(ours, theirs, union):
+    """Pull sources and merge them into their respective place, at least, it attempts to. This is quite buggy. Please report any issues to the GitHub repository :3
+    
+    This is just a handy frontend to `flaura merge`, but this reads from the flaura.toml configuration file rather than from the arguments and options passed."""
     home_path = get_home()
     
     with home_path.open(mode="r") as home_fd:
